f72ed477bd79579d745a6c44a4450e4fed7dd348
Mukundu Murari Sarkar <165672858+mukundumurari@users.noreply.github.com>
2024-04-01 22:01:14 +0600

    Add files via upload

---
+++ class_IRB.R
@@ -0,0 +1,83 @@
+2 + 2
+2 * 2
+2 / 1
+# Hello, welcome to the world
+x <- 3
+x <- c(2,3,5,9)
+sqrt(16)
+2 ** 7
+?reserved
+Y <- "Hello"
+z <- c("Hello, How are you?")
+print(c(x,Y,z))
+log(1)
+log(9)
+# I hate this fucking bangladesh.
+4 - 3
+
+
+#Variables
+melon <- c(2,3,5,6,7)
+a <- 7L
+b <- "Hello"
+c <- "Bangladesh"
+d <- c('How are you')
+e <- c(T , F)
+f <- 2+4i
+g <- 40
+h <- c("Bangladesh", "Australia", "India", "USA", "Japan")
+
+#Checking Data Types
+class(melon)
+nchar(h)
+class(d)
+class(e)
+class(h)
+class(melon)
+
+typeof(melon)
+typeof(g)
+typeof(h)
+
+
+#Working with string data
+nchar(d)
+paste(b , c , d)
+toupper(h)
+tolower(h)
+
+
+#Storing variables
+i <- toupper(h)
+j <- paste(b , c , d)
+print(j)
+print(i)
+
+#Operator
+4 == 4
+4 != 4
+3 < 2
+5 > 3
+4 != 2
+4 >= 2
+5 <= 1
+
+#Logical operator
+2 > 1 | 2 < 1
+4 < 1 | 2 > 4
+4 < 5 & 3 > 4
+4 < 5 & 3 > 2
+5 == 5
+!5 == 5
+
+#Function
+sum(3, 4, 5, 6, 4)
+sum(melon)
+k <- sum(melon)
+sum(5, 5, 5, 5, 5)
+l <- sum(5, 5, 5, 5, 5)
+sqrt(l)
+m <- sqrt(l)
+mean(l)
+mean(melon)
+sd(melon)
